0f1f46cd1511d4c5932501936703d1617eb75610
erfan fany <bktashfany46@gmail.com>
2026-09-25 21:44:03 +0330

    test model rfc

---
+++ test_model.py
@@ -0,0 +1,10 @@
+import joblib
+
+with open("random_farest.joblib", "rb") as model:
+    rfc = joblib.load(model)
+
+test_data = [[2.14, 6.4, 1.7, 3.75]]
+
+result = rfc.predict(test_data)
+
+print(result)
